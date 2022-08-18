af63c4715bfc5a1f56644d619adc033316676a1b
zeroall <heodayeong@gmail.com>
2022-08-18 17:56:43 +0900

    포트폴리오 수정

--- project2/js/javascript.js
+++ project2/js/javascript.js
@@ -26,3 +26,50 @@ office.addEventListener("click", (e) => {
   map.classList.toggle("invisible");
   map1.classList.remove("on");
 });
+
+// kakao map 실행코드
+var container = document.getElementById("map");
+
+var options = {
+  center: new kakao.maps.LatLng(37.5214, 126.9105),
+  level: 3,
+};
+var map = new kakao.maps.Map(container, options);
+// 마커가 표시될 위치입니다
+var markerPosition = new kakao.maps.LatLng(37.5214, 126.9105);
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+  position: markerPosition,
+});
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+// 인포윈도우
+var iwContent = '<div style="padding:5px">    효성중공업 본사 </div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+  iwPosition = new kakao.maps.LatLng(37.5214, 126.9105); //인포윈도우 표시 위치입니다
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+  position: iwPosition,
+  content: iwContent,
+});
+// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+infowindow.open(map, marker);
+
+// location 토글
+$("#officeBtn").on("click", function (e) {
+  e.preventDefault();
+  $("#officeBtn").removeClass("line");
+  $("#factoryBtn").removeClass("line");
+  $("#officeContents").removeClass("on");
+  $("#factoryContents").removeClass("on");
+  $("#officeContents").addClass("on");
+  $(this).addClass("line");
+});
+$("#factoryBtn").on("click", function (e) {
+  e.preventDefault();
+  $("#officeBtn").removeClass("line");
+  $("#factoryBtn").removeClass("line");
+  $("#officeContents").removeClass("on");
+  $("#factoryContents").removeClass("on");
+  $("#factoryContents").addClass("on");
+  $(this).addClass("line");
+});
