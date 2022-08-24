12dc4cb491ac186c87c71e176565fc6f57f23419
zeroall <heodayeong@gmail.com>
2022-08-24 15:12:27 +0900

    포트폴리오 4 수정

--- project4/service-worker.js
+++ project4/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/project4/precache-manifest.12af2a6989fef18f64da4f01bf9be095.js"
+  "/precache-manifest.d5ddc07443d1435d1f3b00a1d2845c00.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/project4/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
