5222b61756497cff3855825c983418c51e9d7e38
zeroall <heodayeong@gmail.com>
2022-08-24 14:35:55 +0900

    프로젝트 4수정

---
+++ project4/precache-manifest.71fbb947234dc3dd35a8bb19e1f721a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d6f2460571d25555344",
+    "url": "/portfolio/project4/static/css/main.cdc07890.chunk.css"
+  },
+  {
+    "revision": "8d6f2460571d25555344",
+    "url": "/portfolio/project4/static/js/main.8d6f2460.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/portfolio/project4/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "be4bdea0645fc07bb067",
+    "url": "/portfolio/project4/static/js/2.be4bdea0.chunk.js"
+  },
+  {
+    "revision": "f92517f0c553f6111e5c",
+    "url": "/portfolio/project4/static/js/runtime~main.f92517f0.js"
+  },
+  {
+    "revision": "53d4894e8d98c2b1c6225689b95711e7",
+    "url": "/portfolio/project4/index.html"
+  }
+];
--- project4/service-worker.js
+++ project4/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdb5b4ae899054d0bc64cf09620fd8fd.js"
+  "/portfolio/project4/precache-manifest.71fbb947234dc3dd35a8bb19e1f721a2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/project4/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
